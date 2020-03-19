4fe1083afecad1dba5e842e29372e1b35d2ac86f
Akutziki <aku.lappalainen@edu.turkuamk.fi>
2020-03-19 14:14:04 +0200

    Initial commit

---
+++ Assets/Scripts/FetchFromHS.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using TMPro;
+
+public class FetchFromHS : MonoBehaviour
+{
+    public bool fethced = false;
+    public int confirmedCases = 0;
+    public int deaths = 0;
+    public int recoveries = 0;
+
+    public string jsonToUse;
+    void Start()
+    {
+        StartCoroutine(GetTexture());
+    }
+    IEnumerator GetTexture()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaData");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            jsonToUse = www.downloadHandler.text;
+            RootObject root = JsonUtility.FromJson<RootObject>(jsonToUse);
+            Debug.Log(root);
+            confirmedCases = root.confirmed.Length;
+            deaths = root.deaths;
+            recoveries = root.recovered.Length;
+            fethced = true;
+        }
+    }
+    [System.Serializable]
+    public class ConfirmedCase
+    {
+        public int id;
+        public string date = null;
+        public string healthCareDistrict = null;
+        public string infectionSourceCountry = null;
+        public string infectionSource = null;
+    }
+    [System.Serializable]
+    public class RecoveredCase
+    {
+        public int id;
+        public string date = null;
+        public string healthCareDistrict = null;
+
+    }
+    [System.Serializable]
+    public class RootObject
+    {
+        public ConfirmedCase[] confirmed;
+        public int deaths;
+        public RecoveredCase[] recovered;
+    }
+}
+
+
+
---
+++ Assets/Scripts/PieChartManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+
+public class PieChartManager : MonoBehaviour
+{
+    public TextMeshProUGUI[] textVersions;
+    public float[] values;
+    public Image circlePrefab;
+    public Color[] circleColors;
+    public FetchFromHS fetchFromHS;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitForLoading());
+    }
+    public IEnumerator WaitForLoading()
+    {
+        while (!fetchFromHS.fethced)
+        {
+            yield return null;
+        }
+        MakeGraph();
+    }
+    public void MakeGraph()
+    {
+        values[0] = fetchFromHS.confirmedCases;
+        values[1] = fetchFromHS.deaths;
+        values[2] = fetchFromHS.recoveries;
+        for (int i = 0; i < 3; i++)
+        {
+            textVersions[i].text = values[i].ToString();
+        }
+
+
+        float total = 0f;
+        float zRotation = 0f;
+        for (int i = 0; i < values.Length; i++)
+        {
+            total += values[i];
+        }
+        for (int i = 0; i < values.Length; i++)
+        {
+            Image newCircle = Instantiate(circlePrefab) as Image;
+            newCircle.transform.SetParent(transform, false);
+            newCircle.color = circleColors[i];
+            newCircle.fillAmount = values[i] / total;
+            newCircle.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, zRotation));
+            zRotation -= newCircle.fillAmount * 360f;
+        }
+    }
+
+
+}
