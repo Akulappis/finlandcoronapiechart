672711845486ee49d8fa0f5eed26606d62340060
Akutziki <aku.lappalainen@edu.turkuamk.fi>
2020-03-26 19:05:50 +0200

    Added map that shows progress regionally

--- Assets/Scripts/FetchFromHS.cs
+++ Assets/Scripts/FetchFromHS.cs
@@ -12,6 +12,8 @@ public class FetchFromHS : MonoBehaviour
     public int recoveries = 0;
 
     public string jsonToUse;
+
+    public int[] infectionsPerRegion = new int[20];
     void Start()
     {
         StartCoroutine(GetTexture());
@@ -33,8 +35,79 @@ public class FetchFromHS : MonoBehaviour
             confirmedCases = root.confirmed.Length;
             deathCases = root.deaths.Length;
             recoveries = root.recovered.Length;
-            fethced = true;
+            CalculateByArea(root.confirmed);
         }
+    }
+    public void CalculateByArea(ConfirmedCase[] confirmedCases)
+    {
+        foreach (var item in confirmedCases)
+        {
+            switch (item.healthCareDistrict)
+            {
+                case "Lappi":
+                    infectionsPerRegion[0]++;
+                    break;
+                case "Länsi-Pohja":
+                    infectionsPerRegion[1]++;
+                    break;
+                case "Pohjois-Pohjanmaa":
+                    infectionsPerRegion[2]++;
+                    break;
+                case "Kainuu":
+                    infectionsPerRegion[3]++;
+                    break;
+                case "Keski-Pohjanmaa":
+                    infectionsPerRegion[4]++;
+                    break;               
+                case "Pohjois-Savo":
+                    infectionsPerRegion[5]++;
+                    break;
+                case "Pohjois-Karjala":
+                    infectionsPerRegion[6]++;          
+                    break;
+                case "Vaasa":
+                    infectionsPerRegion[7]++;
+                    break;
+                case "Etelä-Pohjanmaa":
+                    infectionsPerRegion[8]++;
+                    break;
+                case "Keski-Suomi":
+                    infectionsPerRegion[9]++;
+                    break;
+                case "Etelä-Savo":
+                    infectionsPerRegion[10]++;
+                    break;
+                case "Itä-Savo":
+                    infectionsPerRegion[11]++;
+                    break;
+                case "Pirkanmaa":
+                    infectionsPerRegion[12]++;
+                    break;
+                case "Satakunta":
+                    infectionsPerRegion[13]++;
+                    break;
+                case "Päijät-Häme":
+                    infectionsPerRegion[14]++;
+                    break;
+                case "Kymenlaakso":
+                    infectionsPerRegion[15]++;
+                    break;
+                case "Kanta-Häme":
+                    infectionsPerRegion[16]++;
+                    break;
+                case "Etelä-Karjala":
+                    infectionsPerRegion[17]++;
+                    break;
+                case "HUS":
+                    infectionsPerRegion[18]++;
+                    break;
+                case "Varsinais-Suomi":
+                    infectionsPerRegion[19]++;
+                    break;
+            }
+        }
+                    fethced = true;
+
     }
     [System.Serializable]
     public class ConfirmedCase
---
+++ Assets/Scripts/Progress.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class Progress : MonoBehaviour
+{
+    public FetchFromHS dataScript;
+    public Slider slider;
+    public Gradient gradient;
+    public Image image;
+    public float fillValue = 0;
+    public float currentPercentage = 0;
+    public int id;
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = gameObject.GetComponent<Image>();
+        StartCoroutine(ChangeColor());
+    }
+    public IEnumerator ChangeColor()
+    {
+        while(!dataScript.fethced){
+            yield return null;
+        }
+        
+        fillValue = dataScript.infectionsPerRegion[id];
+        currentPercentage = (fillValue) / 100;
+        image.color = gradient.Evaluate(currentPercentage);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
